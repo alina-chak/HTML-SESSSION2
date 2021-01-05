d641e794b919f3f3baff0258c0feae623e75f89f
Alina Banerjee <alina.banerjee@gdgu.org>
2021-01-05 10:37:51 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+<html>
+  <head><title>This is your friend</title></head>
+  <body>
+    <script type="text/javascript">
+    document.write("this is my new file")
+  </script>
+  </body>
+  </html>
